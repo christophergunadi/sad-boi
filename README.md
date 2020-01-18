94230dbfa366445cd60a87272653b5b85b10ae8e
Christopher Gunadi <christopher51333@gmail.com>
2020-01-19 05:18:09 +0900

    Simple refresh function to detect smile every 5 seconds

--- README.md
+++ README.md
@@ -11,8 +11,6 @@ Create `.env` file in project root directory:
 FACE_SUBSCRIPTION_KEY=<your subscription key>
 ```
 
-This repo provides a template for a Python/Flask app that streams images from the users webcam to a video player on an HTML page. Then when the use clicks a button, it will capture the image and POST it to a route in the Flask app. This route will send the image to the Azure Face API for face detection
-
 ## Acknowledgements
 Used hackathon [template](https://github.com/jimbobbennett/Hackathon-CaptureImageForFaceDetection)
 
--- app.py
+++ app.py
@@ -15,6 +15,19 @@ def home():
     page_data = {}
     return render_template('home.html', page_data = page_data)
 
+def is_happy(emotion):
+    emotions = {}
+    emotions['anger'] = emotion.anger
+    emotions['contempt'] = emotion.contempt
+    emotions['disgust'] = emotion.disgust
+    emotions['fear'] = emotion.fear
+    emotions['happiness'] = emotion.happiness
+    emotions['neutral'] = emotion.neutral
+    emotions['sadness'] = emotion.sadness
+    emotions['surprise'] = emotion.surprise
+    best_emotion = max(zip(emotions.values(), emotions.keys()))[1]
+    return best_emotion == 'happiness'
+
 @app.route('/process_image', methods=['POST'])
 def check_results():
     # Get the JSON passed to the request and extract the image
@@ -25,15 +38,15 @@ def check_results():
 
     # Send the image to the Face API service
     # This gets all the possible attributes
-    face_attributes = list(map(lambda c: c.value, models.FaceAttributeType))
+    # face_attributes = list(map(lambda c: c.value, models.FaceAttributeType))
     faces = face_client.face.detect_with_stream(image,
-                                                return_face_attributes=face_attributes)
-
-    ####################################################
-    #                                                  #
-    # Add your code here to process the detected faces #
-    #                                                  #
-    ####################################################
-
-    # Return a result
-    return jsonify({})
+                                                return_face_attributes=['emotion'])
+    
+    if len(faces) >= 1 and is_happy(faces[0].face_attributes.emotion):
+        return jsonify({
+            'message': 'What a nice smile 😍🥰🌈🔥'
+        })
+    else:
+        return jsonify({
+            'message': 'not happy 😢'
+        })
